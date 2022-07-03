7767816b80e09fffe92939eea41615ddc61b20be
DmytryM <28marchuk28@i.ua>
2022-07-03 23:47:41 +0300

    Test

---
+++ Django_NuxtJS/tasks.py
@@ -0,0 +1,12 @@
+# x = input().split(',')
+# y = int(input())
+# for i in range(len(x)):
+#     for j in range(len(x)):
+#         if int(x[i]) + int(x[j]) == y and i != j:
+#             print(f"[{i}, {j}]")
+#             exit()
+
+
+#--------------------2-------------------
+
+
