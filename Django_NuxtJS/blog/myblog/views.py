6b325531189d5ada3d1cd0a0b5014de59c41a203
DmytryM <28marchuk28@i.ua>
2022-07-06 00:53:26 +0300

    Tasks

--- Django_NuxtJS/blog/myblog/views.py
+++ Django_NuxtJS/blog/myblog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'myblog/index.html'
+        )
