b430b0bca187ddca9c7ae3176a0f656e34c026ea
Dmytry <28marchuk28@i.ua>
2022-09-25 01:33:52 +0300

    Tasks

--- Django_NuxtJS/tasks.py
+++ Django_NuxtJS/tasks.py
@@ -116,18 +116,4 @@
 
 # --------------------16-------------------
 
-import torch
-import torch.optim as optim
-import torch.nn as nn
-from torchviz import make_dot
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# Our data was in Numpy arrays, but we need to transform them into PyTorch's Tensors
-# and then we send them to the chosen device
-x_train_tensor = torch.from_numpy(x_train).float().to(device)
-y_train_tensor = torch.from_numpy(y_train).float().to(device)
-
-# Here we can see the difference - notice that .type() is more useful
-# since it also tells us WHERE the tensor is (device)
-print(type(x_train), type(x_train_tensor), x_train_tensor.type())
