07c8d5979d813a1fd8758c8d96286d68185ff130
Marchuk <28marchuk28@i.ua>
2023-01-23 01:04:52 +0200

    Tasks

--- Django_NuxtJS/ii_start/2.2.py
+++ Django_NuxtJS/ii_start/2.2.py
@@ -14,3 +14,9 @@
 # ------------------------------------------5
 # a, b = int(input()), int(input())
 # print((a % b) + (b//a))
+# ------------------------------------------6
+a, b, c = map(int, input().split())
+if a + b == c:
+    print(True)
+else:
+    print(False)
