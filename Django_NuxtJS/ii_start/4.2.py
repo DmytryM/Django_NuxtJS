3d59f4b5c50c153d21ea0b16c83518e2691eb567
Marchuk <28marchuk28@i.ua>
2023-01-29 20:34:22 +0200

    Tasks

---
+++ Django_NuxtJS/ii_start/4.2.py
@@ -0,0 +1,29 @@
+# -------------------------------1
+# def almost_double_factorial(n):
+#     res = 1
+#     for i in range(n + 1):
+#         if i % 2 != 0:
+#             res *= i
+#     return res
+#
+#
+# res = almost_double_factorial(int(input()))
+# print(res)
+# -------------------------------2
+# count = -1
+# def calls_num():
+#     global count
+#     count += 1
+#     return count
+# print(calls_num())
+# print(calls_num())
+# print(calls_num())
+# print(calls_num())
+# -------------------------------3
+count = 0
+def cmp_prev(val):
+    global count
+    res = []
+    res.append(count)
+    if
+    return
