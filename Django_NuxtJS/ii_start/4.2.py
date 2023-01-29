d76af5c7d715ec117e58a8189d81d8a197491383
Marchuk <28marchuk28@i.ua>
2023-01-30 01:39:31 +0200

    Tasks

--- Django_NuxtJS/ii_start/4.2.py
+++ Django_NuxtJS/ii_start/4.2.py
@@ -20,10 +20,35 @@
 # print(calls_num())
 # print(calls_num())
 # -------------------------------3
-count = 0
-def cmp_prev(val):
-    global count
-    res = []
-    res.append(count)
-    if
-    return
+# nums = []
+# def cmp_prev(val):
+#     global nums
+#     if len(nums) == 0:
+#         if val != 0:
+#             nums.append(val)
+#             return True
+#         elif val == 0:
+#             nums.append(val)
+#             return False
+#     elif 2 > len(nums) > 0:
+#         if val > nums[len(nums) - 1]:
+#             nums.append(val)
+#             return True
+#         else:
+#             nums.append(val)
+#             return False
+#     elif len(nums) >= 2:
+#         if val > (nums[len(nums) - 2] + nums[len(nums) - 1]):
+#             nums.append(val)
+#             return True
+#         else:
+#             nums.append(val)
+#             return False
+#
+# print(cmp_prev(2))
+# print(nums)
+# print(cmp_prev(0))
+# print(nums)
+# print(cmp_prev(3))
+# print(nums)
+# -------------------------------3
