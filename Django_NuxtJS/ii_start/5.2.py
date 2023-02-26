9fe175c69278726072d821e17c923866a657f23b
Marchuk <28marchuk28@i.ua>
2023-02-27 01:05:25 +0200

    Tasks

--- Django_NuxtJS/ii_start/5.2.py
+++ Django_NuxtJS/ii_start/5.2.py
@@ -10,16 +10,28 @@ class MyNum:
         return self.val
 
 # -------------------------------2
-class MyQueue:
-    def __init__(self):
+# class MyQueue:
+#     def __init__(self):
+#         self.stack = []
+#
+#     def push(self, val):
+#         self.stack.append(val)
+#
+#     def front(self):
+#         return self.stack[0]
+#
+#     def pop(self):
+#         del self.stack[0]
+
+# -------------------------------3
+class Neuron:
+
+    def __init__(self, w, f = lambda x: x):
         #YOUR CODE HERE
 
-    def push(self, val):
+    def forward(self, x):
         #YOUR CODE HERE
 
-    def front():
-        #YOUR CODE HERE
-
-    def pop():
+    def backlog(self):
         #YOUR CODE HERE
 
