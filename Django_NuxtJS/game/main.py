d175177d2503aa310b5d9f7f0c5b803cf17059bf
Marchuk <28marchuk28@i.ua>
2023-03-21 23:23:56 +0200

    1game

--- Django_NuxtJS/game/main.py
+++ Django_NuxtJS/game/main.py
@@ -1,43 +1,62 @@
 import pygame
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
+import random
 
 pygame.init()
+
+FPS = pygame.time.Clock()
 
 screen = width, height = 800, 600
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
+GREEN = 0, 255, 0
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
 ball.fill(WHITE)
 ball_rect = ball.get_rect()
-ball_speed = 1
+ball_speed = 5
 
 
 def create_enemy():
     enemy = pygame.Surface((20, 20))
     enemy.fill(RED)
-    enemy_rect = pygame.Rect(width, 100, *enemy.get_size())
-    enemy_speed = 1
+    enemy_rect = pygame.Rect(width, random.randint(0, height), *enemy.get_size())
+    enemy_speed = random.randint(4, 6)
     return [enemy, enemy_rect, enemy_speed]
+
+
+def create_bonus():
+    bonus = pygame.Surface((20, 20))
+    bonus.fill(GREEN)
+    bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
+    bonus_speed = random.randint(4, 6)
+    return [bonus, bonus_rect, bonus_speed]
 
 
 CREATE_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
 
+CREATE_BONUS = pygame.USEREVENT + 2
+pygame.time.set_timer(CREATE_BONUS, 2500)
+
 enemies = []
+bonuses = []
 
 is_working = True
 
 while is_working:
+    FPS.tick(60)
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working = False
         if event.type == CREATE_ENEMY:
             enemies.append(create_enemy())
+        if event.type == CREATE_BONUS:
+            enemies.append(create_bonus())
 
     pressed_keys = pygame.key.get_pressed()
 
@@ -48,16 +67,29 @@ while is_working:
         enemy[1] = enemy[1].move(-enemy[2], 0)
         main_surface.blit(enemy[0], enemy[1])
 
-    if pressed_keys[K_DOWN]:
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+        if ball_rect.colliderect(enemy[1]):
+            enemies.pop(enemies.index(enemy))
+
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(0, bonus[2])
+        main_surface.blit(bonus[0], bonus[1])
+
+        if bonus[1].top >= height:
+            bonuses.pop(bonuses.index(bonus))
+
+    if pressed_keys[K_DOWN] and not ball_rect.bottom >= height:
         ball_rect = ball_rect.move(0, ball_speed)
 
-    if pressed_keys[K_UP]:
+    if pressed_keys[K_UP] and not ball_rect.top <= 0:
         ball_rect = ball_rect.move(0, -ball_speed)
 
-    if pressed_keys[K_RIGHT]:
+    if pressed_keys[K_RIGHT] and not ball_rect.right >= width:
         ball_rect = ball_rect.move(ball_speed, 0)
 
-    if pressed_keys[K_LEFT]:
+    if pressed_keys[K_LEFT] and not ball_rect.left <= 0:
         ball_rect = ball_rect.move(-ball_speed, 0)
 
     pygame.display.flip()
