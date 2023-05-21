b2cdac80dd859db6555631f454382e3418c33eb3
Marchuk <28marchuk28@i.ua>
2023-05-22 01:52:32 +0300

    1game

--- Django_NuxtJS/Diploma/Diploma/main/views.py
+++ Django_NuxtJS/Diploma/Diploma/main/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Home',
+        'values': ['Some', 'hello', '123']
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
