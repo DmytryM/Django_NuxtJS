dc47cb51ee0796398eebebd446a92116ae4e0397
Marchuk <28marchuk28@i.ua>
2023-05-27 00:30:43 +0300

    1game

--- Django_NuxtJS/Diploma/Diploma/news/views.py
+++ Django_NuxtJS/Diploma/Diploma/news/views.py
@@ -4,3 +4,7 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    return render(request, 'news/create.html')
