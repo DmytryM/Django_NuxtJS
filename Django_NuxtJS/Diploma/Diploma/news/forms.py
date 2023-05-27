7a0bb98491957b0346d4a712c29cda12a784e1d6
Marchuk <28marchuk28@i.ua>
2023-05-28 00:29:31 +0300

    1game

--- Django_NuxtJS/Diploma/Diploma/news/forms.py
+++ Django_NuxtJS/Diploma/Diploma/news/forms.py
@@ -1,11 +1,11 @@
 from .models import Articles
 from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
+
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
         fields = ['title', 'anons', 'full_text', 'date']
-
         widgets = {
             "title": TextInput(attrs={
                 'class': 'form-control',
@@ -17,7 +17,8 @@ class ArticlesForm(ModelForm):
             }),
             "date": DateTimeInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Дата'
+                'placeholder': 'Дата',
+                'type': 'datetime-local'
             }),
             "full_text": Textarea(attrs={
                 'class': 'form-control',
--- Django_NuxtJS/Diploma/Diploma/news/views.py
+++ Django_NuxtJS/Diploma/Diploma/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -8,12 +9,40 @@ def news_home(request):
     return render(request, 'news/news_home.html', {'news': news})
 
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Mistake'
+
     form = ArticlesForm()
 
     data = {
-        'form': form
+        'form': form,
+        'error': error
     }
 
-    return render(request, 'news/create.html')
-
+    return render(request, 'news/create.html', data)
