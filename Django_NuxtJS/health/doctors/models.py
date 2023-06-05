5b05e41d774cbdb2c0deccee85f79d32eb8a648f
Marchuk <28marchuk28@i.ua>
2023-06-05 21:35:37 +0300

    1game

--- Django_NuxtJS/health/doctors/models.py
+++ Django_NuxtJS/health/doctors/models.py
@@ -6,18 +6,19 @@ from django.urls import reverse
 
 class Doctors(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Час створення")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Час зміни")
     is_published = models.BooleanField(default=True, verbose_name="Публікація")
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категорії")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категорії")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Лікарі'
@@ -27,6 +28,7 @@ class Doctors(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Категорія")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
--- Django_NuxtJS/health/doctors/views.py
+++ Django_NuxtJS/health/doctors/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -13,10 +13,8 @@ menu = [
 
 def index(request):
     posts = Doctors.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
@@ -44,20 +42,28 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Відображення {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Doctors, slug=post_slug)
+
+    context = {
+
+        'post': post,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'doctors/post.html', context=context)
 
 
 def show_category(request, cat_id):
     posts = Doctors.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id,
